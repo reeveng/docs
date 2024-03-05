ca77022d0720e695adb06ac9622ed44d6fe5c7b9
juicemitapfelndrin <juicemitapfelndrin@gmail.com>
2024-03-05 22:54:48 +0100

    update

--- sendOutMailScript.js
+++ sendOutMailScript.js
@@ -5,16 +5,16 @@ const folderPath = "src/lib/blogposts";
 
 // Get list of newly added files
 const addedFiles = execSync("git diff-tree --name-only --diff-filter=A -r HEAD").toString().split('\n');
-console.log(addedFiles)
+console.log({ addedFiles })
 
-// MongoDB database name
 const dbName = process.env.MONGO_DB_NAME;
-
-// MongoDB collection name
+const dbUrl = process.env.MONGO_URL
 const collectionName = process.env.COLLECTION_NOTIFY;
 
+console.log({ collectionName, dbUrl, dbName })
+
 async function main() {
-    const client = new MongoClient(process.env.MONGO_URL);
+    const client = new MongoClient(dbUrl);
 
     try {
         // Connect to the MongoDB server
@@ -22,36 +22,20 @@ async function main() {
 
         // Loop through each newly added file
         for (const file of addedFiles) {
-            console.log(file)
+            console.log({ file })
             // Check if the file is in the specified folder
             if (file.startsWith(folderPath)) {
                 console.log("Newly added file:", file);
 
-                // MongoDB aggregate pipeline
-                const pipeline = [
-                    {
-                        $match: {
-                            // Add your query criteria here if any
-                        }
-                    },
-                    {
-                        $group: {
-                            _id: null,
-                            emailAddresses: { $push: "$emailAddress" }
-                        }
-                    },
-                    {
-                        $project: {
-                            _id: 0,
-                            emailAddresses: 1
-                        }
-                    }
-                ];
-
                 // Execute MongoDB aggregate query
                 const collection = client.db(dbName).collection(collectionName);
-                const result = await collection.aggregate(pipeline).toArray();
-                console.log(result)
+                const result = await collection.find({}, {
+                    $project: {
+                        _id: 0,
+                        emailAddresses: 1
+                    }
+                }).toArray();
+                console.log({ result })
 
                 // Extract email addresses from MongoDB query result
                 // const emailAddresses = result[0].emailAddresses;
